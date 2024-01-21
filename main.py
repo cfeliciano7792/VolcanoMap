3c632b1c9a592f5b3c565e212f37ec6e1a8c97f7
cfeliciano <cfeliciano7792@gmail.com>
2024-01-21 15:09:46 -0500

    created first marker

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import folium
 
-map = folium.Map(location=[38.58, -99.09], zoom_start=5)
+
+map = folium.Map(location=[38.58, -99.09],
+                 zoom_start=5,
+                 tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
+                 attr='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, '
+                      'GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS '
+                      'User Community')
+
+# Add a single marker
+# map.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a marker", icon=folium.Icon(color='green')))
+
+# Creates  feature group.
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a marker", icon=folium.Icon(color='green')))
+map.add_child(fg)
+
 map.save("Map1.html")
