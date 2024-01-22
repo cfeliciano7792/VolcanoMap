69364adc9ecfdeaae0c7d6e08c4bee1b6c551921
cfeliciano <cfeliciano7792@gmail.com>
2024-01-21 21:34:27 -0500

    Removed Duplicates

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import pandas
 data = pandas.read_csv("VolcanoMap.txt")
 lat = list(data["Latitude"])
 lon = list(data["Longitude"])
-
-
+elevation = list(data["Elevation"])
+name = list(data["Name"])
+country = list(data["Country"])
+vol_type = list(data["Type"])
 
 map = folium.Map(location=[38.58, -99.09],
                  zoom_start=5,
@@ -13,6 +15,7 @@ map = folium.Map(location=[38.58, -99.09],
                  attr='Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, '
                       'GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS '
                       'User Community',
+                 world_copy_jump=True
                  )
 
 # Add a single marker
@@ -21,8 +24,15 @@ map = folium.Map(location=[38.58, -99.09],
 # Creates  feature group.
 fg = folium.FeatureGroup(name="My Map")
 # iterate through two lists at once. Need to use zip function
-for lt, ln in zip(lat,lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi I am a marker", icon=folium.Icon(color='green')))
+for lt, ln, el, v_name, v_country, vol_t in zip(lat, lon, elevation, name, country, vol_type):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(f"""<h3 style="font-size:16px;">{v_name}</h3>
+                                                                    Country = {v_country} <br>
+                                                                    Type = {vol_t} <br>
+                                                                    Elevation = {el} m <br>
+                                                                    <a href='https://www.google.com/search?q={v_name} +
+                                                                    volcano' 
+                                                                    target='_blank'>Learn More!</a>
+                    """, max_width=len(f"name= {name}")*20), icon=folium.Icon(color='green')))
 map.add_child(fg)
 
 map.save("Map1.html")
